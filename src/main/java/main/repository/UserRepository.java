1a795d84f1f0cc3ca9ef8fae083263cfa4d1be73
KMB <burduninkonstantin@gmail.com>
2021-03-10 13:16:55 +0300

    postById views fixed (part IV)

--- src/main/java/main/repository/UserRepository.java
+++ src/main/java/main/repository/UserRepository.java
@@ -9,6 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    @Query(value = "SELECT * FROM users WHERE users.email = :email", nativeQuery = true)
-    User getUserByEmail(@Param("email") String email);
+    User findByEmail(String email);
 }
