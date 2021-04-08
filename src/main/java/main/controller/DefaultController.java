924a200055f2aba4ff7e145c70b99eea14dbac42
KMB <burduninkonstantin@gmail.com>
2021-04-08 17:54:52 +0300

    solve 404 problem with password restore link (part V)

--- src/main/java/main/controller/DefaultController.java
+++ src/main/java/main/controller/DefaultController.java
@@ -2,6 +2,7 @@ package main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -10,5 +11,10 @@ public class DefaultController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\.]*}")
+    public String redirectToIndex() {
+        return "forward:/";
     }
 }
